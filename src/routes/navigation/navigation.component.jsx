760cae069c176b1b051858c6f558f3e15edcd62b
Danylo Perepelitsyn <danyloperepelitsyn@daniel.home>
2022-10-06 19:20:47 -0400

    initial commit

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,41 @@
+import { Fragment, useContext } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+import { UserContext } from '../../contexts/user.context';
+
+import { ReactComponent as CrwnLogo } from '../../assets/hms-logo.svg';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            MENUS
+          </Link>
+
+          {currentUser ? (
+            <span className='nav-link' onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className='nav-link' to='/auth'>
+              SIGN IN
+            </Link>
+          )}
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
